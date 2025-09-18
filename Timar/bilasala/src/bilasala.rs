60d8a5844c239f13bf9cdfb9a87d3a04d3fe7efd
gestskoli <ges@tskoli.is>
2025-09-18 13:19:50 +0000

    búið að skipta for út fyir iter

--- Timar/bilasala/src/bilasala.rs
+++ Timar/bilasala/src/bilasala.rs
@@ -31,45 +31,68 @@ impl Bilasala {
         Ok(self.bilar.push(Bill::new(id, gerd, tegund, verd)?))
     }
 
-    pub fn skoda_bil_med_id(&self, id: u32) {
-        for bill in &self.bilar {
+    pub fn skoda_bil_med_id(&self, id: u32) -> Result<String, String> {
+        if let Some(b) = self.bilar.iter().find(|bill| bill.id() == id) {
+            Ok(b.to_string())
+        } else {
+            Err(format!("Fann engan bíl með id: {}", id))
+        }
+        
+        /* for bill in &self.bilar {
             if bill.id() == id {
                 println!("{}", bill);
                 return;
             }
         }
-        println!("Fann engan bíl með id: {}", id)
+        println!("Fann engan bíl með id: {}", id) */
     }
 
-    pub fn eyda_bil_med_id(&mut self, id: u32) {
-        for idx in 0..(self.bilar.len()) {
+    pub fn eyda_bil_med_id(&mut self, id: u32) -> Result<(), String> {
+        if let Some(idx) = self.bilar
+                                      .iter()
+                                      .position(|bill| bill.id() == id) {
+            self.bilar.remove(idx);
+            Ok(())
+        } else {
+            Err(format!("Fann engan bíl með id: {}", id))
+        }
+/*         for idx in 0..(self.bilar.len()) {
             if self.bilar[idx].id() == id {
                 self.bilar.remove(idx);
                 println!("Eyddi bíl nr: {}", id);
                 return;
             }
         }
-        println!("Fann engan bíl með id: {}", id)
+        println!("Fann engan bíl með id: {}", id) */
     }
 
-    pub fn breyta_verdi_a_bil_med_id(&mut self, id: u32, nytt_verd: u32) {
-        for bill in &mut self.bilar {
+    pub fn breyta_verdi_a_bil_med_id(&mut self, id: u32, nytt_verd: u32) -> Result<(), String> {
+        if let Some(b) = self.bilar.iter_mut().find(|bill| bill.id() == id) {
+            b.set_verd(nytt_verd);
+            Ok(())
+        } else {
+            Err(format!("Fann engan bíl með id: {}", id))
+        }
+        /* for bill in &mut self.bilar {
             if bill.id() == id {
                 bill.set_verd(nytt_verd);
                 println!("Breytti verði á bíl nr: {}", id);
                 return;
             }
         }
-        println!("Fann engan bíl með id: {}", id)
+        println!("Fann engan bíl með id: {}", id) */
     }
 
-    pub fn prenta_verd_allra_bila(&self) {
-        let mut heildarverd = 0_u64;
+    pub fn prenta_verd_allra_bila(&self) -> u64 {
+        self.bilar
+            .iter()           // heildarverd = heildarverd + bill.verd()
+            .fold(0, |heildarverd, bill| heildarverd + bill.verd() as u64)
+/*         let mut heildarverd = 0_u64;
         for bill in &self.bilar {
             heildarverd += bill.verd() as u64;
         }
         println!("Heildarverð: {}, meðalverð: {:.2}", 
-                    heildarverd, heildarverd as f32 / self.bilar.len() as f32)
+                    heildarverd, heildarverd as f32 / self.bilar.len() as f32) */
     }
 }
 
