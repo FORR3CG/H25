643e8c6a29a7d4bc0ed5c921b2c5db171a4580b6
gestskoli <ges@tskoli.is>
2025-09-18 14:18:49 +0000

    notendaviðmót klárt

--- Timar/bilasala/src/main.rs
+++ Timar/bilasala/src/main.rs
@@ -2,38 +2,92 @@ mod bill;
 mod tegund;
 mod bilasala;
 
+use std::io::Write;
+
 use crate::bill::Bill;
 use bilasala::Bilasala;
 
 fn main() {
-    //let b = Bill::new(101, "Toyota", "fb", 1000);
-    // "101 Volvo j 2000"
+    // hjálp
+    // hætta
     // skrá Toyota fb 1000 
     // selja 101
     // breyta 101 2000 
+    // prenta verð
+    // prenta allt
+    // prenta 101
     let mut bs = Bilasala::new();
-    if let Err(e) = bs.skra("Nissan fb 2000") {
-        println!("{}", e)
+    loop {
+        print!(">>> ");
+        std::io::stdout().flush().expect("Gat ekki flush-að!");
+        let mut inntak = String::new();
+        std::io::stdin()
+            .read_line(&mut inntak)
+            .expect("Gat ekki lesið frá notanda!");
+        let skipanir: Vec<&str> = inntak.split_whitespace().collect();
+        match skipanir.first() {
+            Some(skipun) => {
+                match skipun.to_lowercase().as_str() {
+                    "hætta" => break,
+                    "hjálp" => println!("Skrifum út hjálpina"),
+                    "skrá" => {
+                        // skrá 101 Volvo fb 1000
+                        if let Err(e) = bs.skra_bil_str(skipanir[1..].join(" ").as_str()) {
+                            println!("{}", e)
+                        }
+                    },
+                    "eyða" | "selja" => {
+                        if let Some(id_str) = skipanir.get(1) {
+                            if let Ok(id) = id_str.parse::<u32>() {
+                                if let Err(e) = bs.eyda_bil_med_id(id) {
+                                    println!("{}", e)
+                                }
+                            } else {
+                                println!("Gat ekki breytt {} í tölu!", id_str)
+                            }
+                        } else {
+                            println!("Fann ekkert id!")
+                        }
+                    },
+                    "prenta" => {
+                        if skipanir.len() != 2 {
+                            println!("Þú verður að segja mér hvað ég á að prenta!")
+                        } else {
+                            if let Ok(id) = skipanir[1].parse::<u32>() {
+                                match bs.skoda_bil_med_id(id) {
+                                    Ok(bill) => println!("{}", bill),
+                                    Err(villa) => println!("{}", villa),
+                                }
+                            } else {
+                                match skipanir[1] {
+                                    "allt" => println!("{}", bs),
+                                    "verð" => println!("{}", bs.prenta_verd_allra_bila()),
+                                    _ => println!("Kann ekki að prenta '{}'!", skipanir[1]),
+                                }
+                            }
+                        }
+                    },
+                    "breyta" => {
+                        if skipanir.len() != 3 {
+                            println!("Ég þarf að fá id og verð!")
+                        } else {
+                            if let Ok(id) = skipanir[1].parse::<u32>() {
+                                if let Ok(verd) = skipanir[2].parse::<u32>() {
+                                    if let Err(villa) = bs.breyta_verdi_a_bil_med_id(id, verd) {
+                                        println!("{}", villa)
+                                    }
+                                } else {
+                                    println!("Gat ekki breytt '{}' í verð!", skipanir[2])
+                                }
+                            } else {
+                                println!("Gat ekki breytt '{}' í id!", skipanir[1])
+                            }
+                        }
+                    },
+                    _ => println!("Skil ekki skipunina ´{}´", skipun),
+                }
+            }
+            None => println!("Þú verður að slá eitthvað inn!!"),
+        }
     }
-    bs.skra("Komatsu v 5000");
-    println!("{}", bs);
-    if let Err(e) = bs.skra_bil(101, "Toyota", "bátur", 1000) {
-        println!("{}", e)
-    } else {
-        println!("skráði bíl")
-    }
-    if let Err(e) = Bill::try_from("102 Tesla abc geir") {
-        println!("Villa: {}", e)
-    } else {
-        println!("Bjó til bíl")
-    }
-    bs.skra_bil(102, "Nissan", "j", 1000);
-    bs.skra_bil(103, "Scania", "vörubíll", 1000);
-    bs.skoda_bil_med_id(101);
-    bs.skoda_bil_med_id(999);
-    bs.eyda_bil_med_id(999);
-    bs.eyda_bil_med_id(101);
-    println!("{}", bs);
-    bs.breyta_verdi_a_bil_med_id(103, 5000);
-    bs.prenta_verd_allra_bila();
 }
